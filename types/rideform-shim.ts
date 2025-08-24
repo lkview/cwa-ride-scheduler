c1216b937f181b55ac8476a04039955060f0fec5
lkview <timhennings@gmail.com>
2025-08-24 08:34:21 -0700

    Add files via upload

--- types/rideform-shim.ts
+++ types/rideform-shim.ts
@@ -1,9 +1,1 @@
-// This file makes TypeScript happy for the import in
-// app/ride-events/edit/[id]/page.tsx. It declares that the module
-// "../../../../components/RideForm" has a default export (the component)
-// and a named type export RideEvent. It has **no** runtime effect.
-declare module "../../../../components/RideForm" {
-  const RideForm: any;
-  export default RideForm;
-  export type RideEvent = any;
-}
+export {};
